2ee28a7c8473659a26700d76743300782a3d2625
Rob W <robpwatkins@gmail.com>
2020-04-28 21:01:13 -0500

    got Links in nav all working, props, too

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import Home from './components/Home';
 import Listings from './containers/Listings';
+import Login from './components/Login';
 
 const Router = () => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/listings" component={Listings} />
+      <Route path="/login" component={Login} />
     </Switch>
   )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { AppBar, Toolbar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <AppBar position="static" >
       <Toolbar style={{width: "100%", justifyContent: "space-between",}}>
-        <h5>Austin Small Business</h5>
+        <Link to="/">
+          <h5>Austin Small Business</h5>
+        </Link>
         <ul style={{display: "flex", width:"20%", justifyContent: "space-around", listStyle: "none"}}>
-          <li>Listings</li>
-          <li>Login</li>
+          <Link to="/listings">
+            <li>Listings</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
         </ul>
       </Toolbar>
     </AppBar>
