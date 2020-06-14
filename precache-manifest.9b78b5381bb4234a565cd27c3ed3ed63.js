0714501a3f358e5b590c7d2ec6a61eb000d721d8
Rob W <robpwatkins@gmail.com>
2020-06-14 15:32:00 -0500

    Updates

---
+++ precache-manifest.9b78b5381bb4234a565cd27c3ed3ed63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cbdb6172f33d990a7da79aecad6d76",
+    "url": "/small-business/index.html"
+  },
+  {
+    "revision": "12ab107d116b18cd4e2c",
+    "url": "/small-business/static/css/main.d0903e1b.chunk.css"
+  },
+  {
+    "revision": "b5fc99cc888d5a59bcf7",
+    "url": "/small-business/static/js/2.1e453519.chunk.js"
+  },
+  {
+    "revision": "ef90dec32da5a94b550496958cb808df",
+    "url": "/small-business/static/js/2.1e453519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ab107d116b18cd4e2c",
+    "url": "/small-business/static/js/main.4127c9d8.chunk.js"
+  },
+  {
+    "revision": "b77fcd940bb8ee6340eb",
+    "url": "/small-business/static/js/runtime-main.82fda6ed.js"
+  },
+  {
+    "revision": "115ce94fca71b0d7b6f6d73d40f2fa68",
+    "url": "/small-business/static/media/MagnoliaCafe.115ce94f.png"
+  },
+  {
+    "revision": "c1b0a39632fbfe406f91595d1b45b668",
+    "url": "/small-business/static/media/Polvos.c1b0a396.jpg"
+  },
+  {
+    "revision": "f6ab637676c81b674cd21e6d90d8fb5a",
+    "url": "/small-business/static/media/SpartanPizza.f6ab6376.png"
+  },
+  {
+    "revision": "649cf85abd037bd52ffee29744760c4a",
+    "url": "/small-business/static/media/Tacodeli.649cf85a.png"
+  },
+  {
+    "revision": "b7f1ea27436aa121643a1cadbe97b955",
+    "url": "/small-business/static/media/Torchys.b7f1ea27.png"
+  }
+]);
